8b4d99d65e0aa00ee7a167898ac256b79b0f097b
SarthakB07 <sarthak.bansal@spit.ac.in>
2024-04-06 18:46:36 +0530

    models updated

--- models/appointmentSchema.js
+++ models/appointmentSchema.js
@@ -24,6 +24,12 @@ const appointmentSchema = new mongoose.Schema({
     minLength: [11, "Phone Number Must Contain Exact 11 Digits!"],
     maxLength: [11, "Phone Number Must Contain Exact 11 Digits!"],
   },
+  nic: {
+    type: String,
+    required: [true, "NIC Is Required!"],
+    minLength: [13, "NIC Must Contain Only 13 Digits!"],
+    maxLength: [13, "NIC Must Contain Only 13 Digits!"],
+  },
   dob: {
     type: Date,
     required: [true, "DOB Is Required!"],
